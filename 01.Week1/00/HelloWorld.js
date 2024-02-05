153e6f25b886c69517871b8c09700c98cee4d5ee
Shivansh <shivanshkumar760@gmail.com>
2024-02-05 05:35:29 +0530

    Revert "Console.log detail added in 01->00/HelloWorld.js file"
    This reverts commit 961cd6940ca0ca3a2e1b4c7b5df5c4e4b794eb7e.

--- 01.Week1/00/HelloWorld.js
+++ 01.Week1/00/HelloWorld.js
@@ -1,4 +1,1 @@
-//To Console out a line or a expression onto the terminal we use commamd console.log 
-//where conosle is terminal window this helps us in error rectification and many more
-//conosling out is simply printing or displaying something on terminal
 console.log("Hello World!");
